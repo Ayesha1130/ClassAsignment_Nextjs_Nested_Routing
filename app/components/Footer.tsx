505d0cb4864f1a74e892c36c53d4068bf7a526e2
Ayesha1130 <ayeshaiqbal771@gmail.com>
2024-10-22 11:59:24 +0500

    first commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,18 +1,13 @@
-import React from 'react'
+import React from "react";
 import customM from "../components/customM.module.css";
-
 
 const Footer = () => {
   return (
     <div className={customM.mfooter}>
-
-        
-          <p>Copyright © 2022 Coffee Haven</p>
-          <p>All rights reserved</p>
-        
-      
+      <p>Copyright © 2022 Coffee Haven</p>
+      <p>All rights reserved</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,8 @@
-import About from "./about/page"
-
 import Main from "./components/Main"
-import Services from "./services/page"
-import Contact from "./contact/page"
-
-import Homedelivery from "./services/homedelivery/page"
-
-
 export default function Home(){
   return(
     <div>
-     
-     <Main />
-     <About />
-     <Homedelivery />
-     <Services />
-     <Contact />
-     
+     <Main />   
     </div>
 
   )
